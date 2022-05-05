6670f79027a7331170ff4de6bd608a3a0bb6d5de
Aitor Pérez <aitor.perez@epfl.ch>
2022-05-05 15:06:05 +0200

    Renamed module

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='GraphAI',
+    name='graphai',
     version='0.1.0',
     description='CEDE EPFL Graph AI',
     author='Aitor Pérez',
