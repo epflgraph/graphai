1e1687d8209b23ef9032cd7f41934d40d4628a5a
Aitor Pérez <aitor.perez@epfl.ch>
2022-05-16 10:07:06 +0200

    refactor: deploy prod with uvicorn instead of gunicorn

--- concept_detection/api/deploy-prod.sh
+++ concept_detection/api/deploy-prod.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 if [ "$#" -ge 2 ]; then
-    printf "Too many parameters. Usage: deploy-prod.sh [host]. Example: deploy.sh 192.168.142.120\n";
+    printf "Too many parameters. Usage: deploy-prod.sh [host]. Example: deploy.sh 192.168.140.120\n";
     exit
 fi
 
 if [ "$#" -ge 1 ]; then
-    gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind $1:28800
+    uvicorn main:app --host $1 --port 28800 --reload
+    # gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind $1:28800
 else
-    gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 192.168.140.120:28800
+    uvicorn main:app --host 192.168.140.120 --port 28800 --reload
+    # gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 192.168.140.120:28800
 fi
