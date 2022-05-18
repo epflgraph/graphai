17053cc2e391bfcd154292d4c45ff35eb6b0675c
Aitor Pérez <aitor.perez@epfl.ch>
2022-05-18 12:42:43 +0200

    feat: add support for several batches to overcome elasticsearch default limit of 10000

--- concept_detection/elasticsearch/create.py
+++ concept_detection/elasticsearch/create.py
@@ -8,7 +8,8 @@ db = DB()
 es = ES()
 
 # Get ids of document batch checking which still have
-batch_size = 1000
+n_batches = 4
+batch_size = 10000
 query = {
     'bool': {
         'must': {
@@ -18,35 +19,37 @@ query = {
         }
     }
 }
-r = es._search(query=query, source=['id'], limit=batch_size)
-batch_page_ids = [str(hit['_source']['id']) for hit in r['hits']['hits']]
 
-print(f"Indexing {batch_size} new documents out of {r['hits']['total']['relation']} {r['hits']['total']['value']} pending ones.")
+for i in range(n_batches):
+    r = es._search(query=query, source=['id'], limit=batch_size)
+    batch_page_ids = [str(hit['_source']['id']) for hit in r['hits']['hits']]
 
-pages = db.query_wikipedia_pages(ids=batch_page_ids)
+    print(f"Indexing {batch_size} new documents out of {r['hits']['total']['relation']} {r['hits']['total']['value']} pending ones.")
 
-st = time.time()
-i = 0
-for page in pages:
-    i += 1
-    bar_length = 50
-    done = int(bar_length * i / len(pages))
-    to_do = bar_length - done
-    print(f'\r[{"#" * done}{"." * to_do}]', end='', flush=True)
+    pages = db.query_wikipedia_pages(ids=batch_page_ids)
 
-    stripped_page = strip(page['page_content'])
-    doc = {
-        'id': page['page_id'],
-        'title': page['page_title'],
-        'text': stripped_page['text'],
-        'headings': stripped_page['headings'],
-        'opening_text': stripped_page['opening_text'],
-        'categories': stripped_page['categories']
-    }
-    es.index_doc(doc)
+    st = time.time()
+    i = 0
+    for page in pages:
+        i += 1
+        bar_length = 50
+        done = int(bar_length * i / len(pages))
+        to_do = bar_length - done
+        print(f'\r[{"#" * done}{"." * to_do}]', end='', flush=True)
 
-# Refresh index
-es.refresh()
+        stripped_page = strip(page['page_content'])
+        doc = {
+            'id': page['page_id'],
+            'title': page['page_title'],
+            'text': stripped_page['text'],
+            'headings': stripped_page['headings'],
+            'opening_text': stripped_page['opening_text'],
+            'categories': stripped_page['categories']
+        }
+        es.index_doc(doc)
 
-ft = time.time()
-print(f'\nFinished! Took {ft - st:.2f}s.')
+    # Refresh index
+    es.refresh()
+
+    ft = time.time()
+    print(f'\nFinished! Took {ft - st:.2f}s.')
