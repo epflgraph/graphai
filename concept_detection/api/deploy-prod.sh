35b5bd6349eee87517abfa62c1a7d44494866f1b
Aitor Pérez <aitor.perez@epfl.ch>
2022-05-30 10:38:24 +0200

    build: set gunicorn as default deployment for prod

--- concept_detection/api/deploy-prod.sh
+++ concept_detection/api/deploy-prod.sh
@@ -5,9 +5,9 @@ if [ "$#" -ge 2 ]; then
 fi
 
 if [ "$#" -ge 1 ]; then
-    uvicorn main:app --host $1 --port 28800 --reload
-    # gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind $1:28800
+    # uvicorn main:app --host $1 --port 28800 --reload
+    gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind $1:28800
 else
-    uvicorn main:app --host 192.168.140.120 --port 28800 --reload
-    # gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 192.168.140.120:28800
+    # uvicorn main:app --host 192.168.140.120 --port 28800 --reload
+    gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 192.168.140.120:28800
 fi
