6fff27039686f2a1da6ffab27cd3d756dbf87e02
Aitor Pérez <aitor.perez@epfl.ch>
2022-06-03 10:32:39 +0200

    feat: add stopwatch to track time easily

--- concept_detection/text/io.py
+++ concept_detection/text/io.py
@@ -1,5 +1,6 @@
 import json
 
+
 def pprint(t, indent=0, inline=False, only_first=False):
     if type(t) is dict:
         if inline:
@@ -40,6 +41,26 @@ def pprint(t, indent=0, inline=False, only_first=False):
         print(t)
     else:
         print(f'{" " * indent}{t}')
+
+
+class Colors:
+    pref = '\033['
+    reset = f'{pref}0m'
+
+    codes = {
+        'black': '30m',
+        'red': '31m',
+        'green': '32m',
+        'yellow': '33m',
+        'blue': '34m',
+        'magenta': '35m',
+        'cyan': '36m',
+        'white': '37m',
+    }
+
+
+def cprint(text, color='white', is_bold=False):
+    print(f'{Colors.pref}{1 if is_bold else 0};{Colors.codes.get(color, Colors.codes["white"])}' + text + Colors.reset)
 
 
 def read_json(filename):
