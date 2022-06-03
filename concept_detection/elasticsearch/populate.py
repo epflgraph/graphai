cf2a4e05105b76f9cbb8052b0371b94219dfce70
Aitor Pérez <aitor.perez@epfl.ch>
2022-06-03 15:17:08 +0200

    feat: add parallelization for populate script

--- concept_detection/elasticsearch/populate.py
+++ concept_detection/elasticsearch/populate.py
@@ -1,14 +1,47 @@
+import ray
+
 from concept_detection.interfaces.db import DB
 from concept_detection.interfaces.es import ES
 from concept_detection.text.stripper import strip
-from concept_detection.text.io import ProgressBar
 from concept_detection.time.stopwatch import Stopwatch
 
-db = DB()
-es = ES()
+# Init ray
+ray.init(namespace="populate_elasticsearch", include_dashboard=False, log_to_driver=True)
+
+@ray.remote
+class Actor:
+    def __init__(self):
+        self.es = ES()
+
+    def strip_and_index(self, page_id, page_title, page_content, page_categories):
+        # Strip page content
+        stripped_page = strip(page_content)
+
+        # Prepare document to index
+        doc = {
+            'id': page_id,
+            'title': page_title,
+            'text': stripped_page['text'],
+            'heading': stripped_page['heading'],
+            'opening_text': stripped_page['opening_text'],
+            'auxiliary_text': stripped_page['auxiliary_text'],
+            'file_text': '',
+            'redirect': [],
+            'category': page_categories,
+            'incoming_links': 1,
+            'popularity_score': 1
+        }
+
+        # Index on elasticsearch
+        self.es.index_doc(doc)
+
+
+# Instantiate ray actor list
+n_actors = 16
+actors = [Actor.remote() for i in range(n_actors)]
 
 # Define window size and offset to filter page ids
-window_size = 100
+window_size = 1000
 window_offset = 0
 min_id = window_offset * window_size
 max_id = (window_offset + 1) * window_size
@@ -17,6 +50,7 @@ max_id = (window_offset + 1) * window_size
 sw = Stopwatch()
 
 # Fetch pages from database
+db = DB()
 pages = db.get_wikipages(id_min_max=(min_id, max_id))
 n_pages = len(pages)
 print(f'Got {n_pages} pages (id in [{min_id}, {max_id}])!')
@@ -29,40 +63,21 @@ print(f'Got categories for {len(categories)} pages (id in [{min_id}, {max_id}])!
 # Print time summary
 sw.report('Finished fetching from database')
 
-# Init progressbar to visually keep track of progress
-pb = ProgressBar(n_pages, bar_length=200)
-
 # Reset stopwatch and iterate over all pages
 sw.reset()
+i = 0
+results = []
 for page_id in pages:
-    # Update progressbar
-    pb.update()
-
-    # Extract page data and strip markdown
+    # Extract page data and categories
     page = pages[page_id]
     page_categories = categories.get(page_id, [])
-    stripped_page = strip(page['content'])
 
-    # Prepare document to index
-    doc = {
-        'id': page_id,
-        'title': page['title'],
-        'text': stripped_page['text'],
-        'heading': stripped_page['heading'],
-        'opening_text': stripped_page['opening_text'],
-        'auxiliary_text': stripped_page['auxiliary_text'],
-        'file_text': '',
-        'redirect': [],
-        'category': page_categories,
-        'incoming_links': 1,
-        'popularity_score': 1
-    }
+    # Execute strip_and_index in parallel
+    results.append(actors[i % n_actors].strip_and_index.remote(page_id, page['title'], page['content'], page_categories))
+    i += 1
 
-    # Index on elasticsearch
-    es.index_doc(doc)
-
-# Refresh index
-es.refresh()
+# Wait for the results
+results = ray.get(results)
 
 # Print time summary
 sw.report(f'Finished indexing {n_pages} pages on elasticsearch')
