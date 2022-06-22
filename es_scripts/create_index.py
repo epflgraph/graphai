c823b8aa63625b828dd4cac5ff3c0d6a0714c0e3
Aitor Pérez <aitor.perez@epfl.ch>
2022-06-22 13:29:50 +0200

    refactor: change how elasticsearch indices are created, setting the number of shards explicitly in the script instead of the settings file

--- es_scripts/create_index.py
+++ es_scripts/create_index.py
@@ -2,10 +2,11 @@ from interfaces.es import ES
 
 from utils.text.io import read_json
 
-index = 'wikipages'
+index = 'wikipages_1_shards'
 es = ES(index)
 
 settings = read_json('config/settings.json')
+settings['number_of_shards'] = 1
 mapping = read_json('config/mapping.json')
 
 es.delete_index()
