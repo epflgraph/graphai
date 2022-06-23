6b6fb975271948dbeb201b212fb51cfee872a758
Aitor Pérez <aitor.perez@epfl.ch>
2022-06-23 11:12:35 +0200

    minor: rename wikify local request script

--- concept_detection/requests/bash/wikify-local.sh
+++ concept_detection/requests/bash/wikify-local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$#" -lt 1 ]; then
-    printf "Missing parameters. Usage: wikify-local-base.sh (file.json)+\n";
+    printf "Missing parameters. Usage: wikify-local.sh (file.json)+\n";
     exit
 fi
 
@@ -10,6 +10,6 @@ do
     if [ "$#" -ne 1 ]; then
         printf "$file\n"
     fi
-    curl -X POST http://localhost:28800/wikify?method=base -H 'Content-Type: application/json' --data-binary "@$file";
+    curl -X POST http://localhost:28800/wikify -H 'Content-Type: application/json' --data-binary "@$file";
     printf "\n"
 done
