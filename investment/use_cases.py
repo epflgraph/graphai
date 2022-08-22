9cfd4bc26b1492c093ed8e54383e1d9ed27e35a8
Aitor Pérez <aitor.perez@epfl.ch>
2022-08-22 15:53:28 +0200

    feat: finish function to get top n investment per concept

--- investment/use_cases.py
+++ investment/use_cases.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import numpy as np
 
 from interfaces.db import DB
 
 
-def get_concepts_top_investors(concept_ids):
+def get_concepts_top_investors(concept_ids, n_investors=3):
     # Instantiate db interface to communicate with database
     db = DB()
 
@@ -16,7 +17,17 @@ def get_concepts_top_investors(concept_ids):
     # Extract list of investor ids
     investor_ids = list(investors_concepts['InvestorID'].drop_duplicates())
 
-    investors_concepts_history = get_investor_concept_history(investor_ids, concept_ids)
+    # Build investor-concept history for each pair
+    history = get_investor_concept_history(investor_ids, concept_ids)
+
+    # Keep top `n_investors` for each concept
+    top_investors = {}
+    for concept_id in history:
+        del history[concept_id]['total']
+        concept_top_investor_ids = [investor_id for investor_id, _ in sorted(history[concept_id].items(), key=lambda item: item[1]['total'], reverse=True)]
+        top_investors[concept_id] = {investor_id: history[concept_id][investor_id]['total'] for investor_id in concept_top_investor_ids[:n_investors]}
+
+    return top_investors
 
 
 def get_investor_concept_history(investor_ids, concept_ids):
@@ -75,15 +86,16 @@ def get_investor_concept_history(investor_ids, concept_ids):
     # HISTORY #
     ###########
 
+    # Sort funding rounds by date
     frs = frs.sort_values(by='FundingRoundDate').reset_index(drop=True)
-    print(len(frs))
 
+    # Merge dataframes investors-frs-investees-concepts to have all information together
     df = investors_frs \
         .merge(frs_investees, how='inner', on='FundingRoundID') \
         .merge(investees_concepts, how='inner', on='InvesteeID') \
         .merge(frs, how='inner', on='FundingRoundID')
-    print(len(df))
 
+    # Compute investor-concept history for each pair
     history = {}
     for concept_id in concept_ids:
         history[concept_id] = {}
@@ -91,24 +103,50 @@ def get_investor_concept_history(investor_ids, concept_ids):
         for investor_id in investor_ids:
             history[concept_id][investor_id] = {}
 
+            # Restrict df to current concept and investor
             concept_investor_df = df[(df['InvestorID'] == investor_id) & (df['PageID'] == concept_id)]
 
-            if len(concept_investor_df) == 0:
-                continue
+            # if len(concept_investor_df) == 0:
+            #     continue
 
+            # Iterate over all dates and gather all investments made by current investor in current concept each day.
             for fr_date in concept_investor_df['FundingRoundDate'].drop_duplicates():
                 history[concept_id][investor_id][str(fr_date)] = {}
 
+                # Restrict df to current concept, investor and date
                 concept_investor_date_df = concept_investor_df[concept_investor_df['FundingRoundDate'] == fr_date]
 
+                # Iterate over frs and gather all investments made by current investor in current concept on given day.
+                date_total = 0
                 for fr_id in concept_investor_date_df['FundingRoundID']:
+                    # Restrict df to current concept, investor, date and funding round
                     concept_investor_date_fr_df = concept_investor_date_df[concept_investor_date_df['FundingRoundID'] == fr_id]
 
-                    assert len(concept_investor_date_fr_df) == 1, f'There should be at most one row for investor {investor_id} and funding round {fr_id}, {len(concept_investor_date_fr_df)} found.'
+                    # There should be exactly one row for each combination of parameters
+                    assert len(concept_investor_date_fr_df) == 1, f'There should be exactly one row for investor {investor_id} and funding round {fr_id}, {len(concept_investor_date_fr_df)} found.'
 
-                    history[concept_id][investor_id][str(fr_date)][fr_id] = {'amount': concept_investor_date_fr_df['FundingAmountPerInvestor_USD'].iloc[0]}
+                    # Extract amount and build history
+                    amount = concept_investor_date_fr_df['FundingAmountPerInvestor_USD'].iloc[0]
+                    history[concept_id][investor_id][str(fr_date)][fr_id] = {'amount': amount}
+                    date_total += np.nan_to_num(amount)
 
-    print(history)
+                # Update total investment amount for the given concept, investor and date
+                history[concept_id][investor_id][str(fr_date)]['total'] = date_total
+
+            # Update total investment amount for the given concept and investor
+            history[concept_id][investor_id]['total'] = sum(
+                [history[concept_id][investor_id][d]['total'] for d in history[concept_id][investor_id]]
+            )
+
+        # Update total investment amount for the given concept
+        #   WATCH OUT! This total investment amount only considers investments for which
+        #   we know the investors, and hence will not include amounts coming from investments
+        #   for which we know the amount but not the investors.
+        history[concept_id]['total'] = sum(
+            [history[concept_id][investor_id]['total'] for investor_id in history[concept_id]]
+        )
+
+    return history
 
 
 def main():
