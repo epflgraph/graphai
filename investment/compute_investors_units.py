b4c95f2c18c9f6077add1c31a2e857b993a68bec
Aitor Pérez <aitor.perez@epfl.ch>
2022-11-08 17:07:27 +0100

    feat: rewrite script to compute investors units to match Hilbert space formalism for concept score configurations

--- investment/compute_investors_units.py
+++ investment/compute_investors_units.py
@@ -6,122 +6,193 @@ from interfaces.db import DB
 from utils.breadcrumb import Breadcrumb
 
 
-def mix(x, concepts_concepts):
+def norm(x):
     """
-    One-step mixing of edges through concepts.
+    Computes the norm of the different configurations in x.
 
     Args:
-        x (pd.DataFrame): DataFrame including columns PageID and Score
-        concepts_concepts (pd.DataFrame): Dataframe including columns SourcePageID, TargetPageID and Score
+        x (pd.DataFrame): DataFrame whose columns are key + ['PageID', 'Score']. Each configuration of scores is given
+            by a unique tuple of values for columns in key. For example, if x has columns
+            ['InvestorID', 'PageID', 'Score'], then each value of InvestorID is assumed to index a configuration,
+            given by the columns ['PageID', 'Score'].
 
-    Returns (pd.DataFrame): DataFrame with the same columns as x, the contents of which are the one-step mixing
-        of x following edges in concepts_concepts. This is typically larger than the original DataFrame.
+    Returns (pd.DataFrame): DataFrame with columns key + ['Norm'], containing the norm of each configuration, computed
+        as \sqrt{\sum_{c \in C} X(c)^2}, where C is the set of concepts and X: C \to [0, 1] is a given configuration.
+    """
+
+    # Extract key indexing each configuration
+    key = [c for c in x.columns if c not in ['PageID', 'Score']]
+
+    norms = x.copy()
+
+    # Square all scores to sum them
+    norms['Norm'] = np.square(norms['Score'])
+
+    # Group by key and add the squares of the scores
+    norms = norms.groupby(by=key).aggregate(Norm=('Norm', 'sum')).reset_index()
+
+    # Take the square root to get the norm
+    norms['Norm'] = np.sqrt(norms['Norm'])
+
+    return norms
+
+
+def combine(x, y, pairs):
+    """
+    Combines the configurations in x and y based on the associations in pairs.
+
+    Args:
+        x (pd.DataFrame): DataFrame whose columns are key_x + ['PageID', 'Score']. Each configuration of scores is
+            given by a unique tuple of values for columns in key_x. For example, if x has columns
+            ['InvestorID', 'PageID', 'Score'], then each value of InvestorID is assumed to index a configuration,
+            given by the columns ['PageID', 'Score'].
+        y (pd.DataFrame): DataFrame whose columns are key_y + ['PageID', 'Score']. Each configuration of scores is
+            given by a unique tuple of values for columns in key_y. For example, if y has columns
+            ['InvestorID', 'PageID', 'Score'], then each value of InvestorID is assumed to index a configuration,
+            given by the columns ['PageID', 'Score'].
+        pairs (pd.DataFrame): DataFrame whose columns are key_x + key_y. Configurations in x and y are compared
+            based on the associations in this DataFrame.
+
+    Returns (pd.DataFrame): DataFrame with columns key_x + key_y + ['PageID', 'Score']. For each row in pairs, there is
+        a configuration of scores in the returned DataFrame. The score for each concept is the geometric mean of the
+        scores in x and y.
+    """
+
+    # Extract keys
+    key_x = [c for c in x.columns if c not in ['PageID', 'Score']]
+    key_y = [c for c in y.columns if c not in ['PageID', 'Score']]
+
+    combination = pairs.copy()
+
+    # Merge x and y
+    combination = pd.merge(combination, x, how='inner', on=key_x)
+    combination = pd.merge(combination, y, how='inner', on=(key_y + ['PageID']))
+
+    # Compute scores
+    combination['Score'] = np.sqrt(combination['Score_x'] * combination['Score_y'])
+
+    return combination[key_x + key_y + ['PageID', 'Score']]
+
+
+def mix(x, edges):
+    """
+    Mixes the configurations in x according to the edges in edges.
+
+    Args:
+        x (pd.DataFrame): DataFrame whose columns are key + ['PageID', 'Score']. Each configuration of scores is given
+            by a unique tuple of values for columns in key. For example, if x has columns
+            ['InvestorID', 'PageID', 'Score'], then each value of InvestorID is assumed to index a configuration, given
+            by the columns ['PageID', 'Score'].
+        edges (pd.DataFrame): DataFrame whose columns are ['SourcePageID', 'TargetPageID', 'Score'], which define the
+            weighted edges of the concepts graph.
+
+    Returns (pd.DataFrame): DataFrame with the same columns as x, containing configurations indexed by the same set as
+        x. The score of a concept in the mixed configuration is the arithmetic mean of the geometric means of the
+        configuration score and the edge score in the neighbourhood of the concept.
     """
 
     # Extract key
-    key = [c for c in x.columns if c != 'Score']
+    key = [c for c in x.columns if c not in ['PageID', 'Score']]
+
+    mixed = x.copy()
 
     # Merge both DataFrames through concept-concept edges considering both forward and backward edges
-    x = pd.concat([
+    mixed = pd.concat([
         pd.merge(
-            x,
-            concepts_concepts.rename(columns={'SourcePageID': 'PageID', 'Score': 'EdgeScore'}),
+            mixed,
+            edges.rename(columns={'SourcePageID': 'PageID', 'Score': 'EdgeScore'}),
             how='inner',
             on='PageID'
         ),
         pd.merge(
-            x,
-            concepts_concepts.rename(
-                columns={'TargetPageID': 'PageID', 'SourcePageID': 'TargetPageID', 'Score': 'EdgeScore'}),
+            mixed,
+            edges.rename(columns={'TargetPageID': 'PageID', 'SourcePageID': 'TargetPageID', 'Score': 'EdgeScore'}),
             how='inner',
             on='PageID'
         )
     ])
 
-    # Take the geometric mean of both scores, the original one XXXX-concept and the concept-concept one
-    x['Score'] = np.sqrt(x['Score'] * x['EdgeScore'])
+    # Take the geometric mean of both scores, from the configuration and from the edge
+    mixed['Score'] = np.sqrt(mixed['Score'] * mixed['EdgeScore'])
 
-    # Drop and rename columns to match again the columns of x
-    x.drop(columns=['PageID', 'EdgeScore'], inplace=True)
-    x = x.rename(columns={'TargetPageID': 'PageID'})
+    # Drop and rename columns to match again the original columns
+    mixed = mixed.drop(columns=['PageID', 'EdgeScore'])
+    mixed = mixed.rename(columns={'TargetPageID': 'PageID'})
+    mixed = mixed[key + ['PageID', 'Score']].reset_index(drop=True)
 
     # Compute the scores for each of the new pages by averaging out the scores over all neighbours
-    x.groupby(by=key).aggregate(Score=('Score', 'mean')).reset_index()
+    mixed = mixed.groupby(by=(key + ['PageID'])).aggregate(Score=('Score', 'mean')).reset_index()
 
-    return x
+    # # Filter scores below threshold to avoid memory issues with long tails
+    # mixed = mixed[mixed['Score'] >= 0.2].reset_index(drop=True)
+
+    return mixed
 
 
-def combine(base, x, y):
+def compute_affinities(x, y, pairs, edges):
     """
-    Combines different edge scores through concepts. For the pairs specified in base, it computes a score by combining
-    the scores in x and y.
+    Computes affinity scores between the pairs configurations in x and y indexed in pairs, according to the concepts
+    (edge-weighted) graph specified in edges.
 
     Args:
-        base (pd.DataFrame): DataFrame including the columns in x and y, except PageID and Score
-        x (pd.DataFrame): DataFrame including columns PageID and Score
-        y (pd.DataFrame): DataFrame including columns PageID and Score
+        x (pd.DataFrame): DataFrame whose columns are key_x + ['PageID', 'Score']. Each configuration of scores is
+            given by a unique tuple of values for columns in key_x. For example, if x has columns
+            ['InvestorID', 'PageID', 'Score'], then each value of InvestorID is assumed to index a configuration,
+            given by the columns ['PageID', 'Score'].
+        y (pd.DataFrame): DataFrame whose columns are key_y + ['PageID', 'Score']. Each configuration of scores is
+            given by a unique tuple of values for columns in key_y. For example, if y has columns
+            ['InvestorID', 'PageID', 'Score'], then each value of InvestorID is assumed to index a configuration,
+            given by the columns ['PageID', 'Score'].
+        pairs (pd.DataFrame): DataFrame whose columns are key_x + key_y. Configurations in x and y are compared
+            based on the associations in this DataFrame.
+        edges (pd.DataFrame): DataFrame whose columns are ['SourcePageID', 'TargetPageID', 'Score'], which define the
+            weighted edges of the concepts graph.
 
-    Returns (pd.DataFrame): The base DataFrame with an extra column Score.
+    Returns (pd.DataFrame): DataFrame with columns key_x + key_y + ['Score'], containing the same rows as pairs.
+        For each pair of configuration X and Y, their score is computed as the square root of the ratio of the norm
+        of BX*BY squared and the product of norms of BX and BY. Here BX and BY denote the mixing of X and Y,
+        respectively, and BX*BY denotes the combination of BX and BY.
     """
+
     # Extract keys
     key_x = [c for c in x.columns if c not in ['PageID', 'Score']]
     key_y = [c for c in y.columns if c not in ['PageID', 'Score']]
 
-    # Add squared norm for x (sum of all scores)
-    x = pd.merge(
-        x,
-        x.groupby(by=key_x).aggregate(SqNorm=('Score', 'sum')).reset_index(),
-        how='inner',
+    # Compute mixings BX and BY, combination BX*BY and their norms
+    bx = mix(x, edges)
+    by = mix(y, edges)
+    bxby = combine(bx, by, pairs)
+    norms_bx = norm(bx)
+    norms_by = norm(by)
+    norms_bxby = norm(bxby)
+
+    # Merge norms for BX, BY and BX*BY into pairs DataFrame
+    pairs = pd.merge(
+        pairs,
+        norms_bx.rename(columns={'Norm': 'NormBX'}),
+        how='left',
         on=key_x
     )
 
-    # Add squared norm for y (sum of all scores)
-    y = pd.merge(
-        y,
-        y.groupby(by=key_y).aggregate(SqNorm=('Score', 'sum')).reset_index(),
-        how='inner',
+    pairs = pd.merge(
+        pairs,
+        norms_by.rename(columns={'Norm': 'NormBY'}),
+        how='left',
         on=key_y
     )
 
-    # Merge both into base
-    base = pd.merge(
-        base,
-        x,
-        how='inner',
-        on=key_x
-    )
-    base = pd.merge(
-        base,
-        y,
-        how='inner',
-        on=(key_y + ['PageID'])
+    pairs = pd.merge(
+        pairs,
+        norms_bxby.rename(columns={'Norm': 'NormBXBY'}),
+        how='left',
+        on=(key_x + key_y)
     )
 
-    # Compute affinity score and renormalize it
-    base['Score'] = np.sqrt(base['Score_x'] * base['Score_y']) / np.sqrt(base['SqNorm_x'] * base['SqNorm_y'])
-    base = base.groupby(by=(key_x + key_y)).aggregate(Score=('Score', 'sum')).reset_index()
+    # Compute affinity scores
+    pairs = pairs.fillna(0)
+    pairs['Score'] = np.sqrt(np.square(pairs['NormBXBY']) / (pairs['NormBX'] * pairs['NormBY']))
 
-    base['Score'] = np.sqrt(base['Score'])
-
-    return base
-
-
-def compute_affinities(investors_units, investors_concepts, units_concepts, concepts_concepts):
-
-    investors_units = investors_units.drop(columns=['Score'])
-
-    aaa = combine(investors_units, investors_concepts, units_concepts)
-
-    print(aaa)
-    print(aaa.describe())
-
-    mixed_investors_concepts = mix(investors_concepts, concepts_concepts)
-    mixed_units_concepts = mix(units_concepts, concepts_concepts)
-
-    bbb = combine(investors_units, mixed_investors_concepts, mixed_units_concepts)
-
-    print(bbb)
-    print(bbb.describe())
+    return pairs[key_x + key_y + ['Score']]
 
 
 def main():
@@ -149,7 +220,6 @@ def main():
     )
     units_concepts['Score'] = units_concepts['Score'] / units_concepts['MaxScore']
     units_concepts = units_concepts.drop(columns='MaxScore')
-    unit_ids = list(units_concepts['UnitID'].drop_duplicates())
 
     ############################################################
 
@@ -197,11 +267,42 @@ def main():
     fields = ['SourcePageID', 'TargetPageID', 'NormalisedScore']
     concepts_concepts = pd.DataFrame(db.find(table_name, fields=fields), columns=['SourcePageID', 'TargetPageID', 'Score'])
 
+    # Consider only edges above a threshold to avoid too intensive memory usage
+    # concepts_concepts = concepts_concepts[concepts_concepts['Score'] >= 0.1].reset_index(drop=True)
+
+    ############################################################
+
+    bc.log('Adding year to investors-units...')
+
+    investors_units = pd.merge(
+        investors_concepts[['InvestorID', 'Year']].drop_duplicates(),
+        investors_units,
+        how='left',
+        on='InvestorID'
+    )
+
     ############################################################
 
     bc.log('Computing affinities investors-units...')
 
-    compute_affinities(investors_units, investors_concepts, units_concepts, concepts_concepts)
+    investors_units = investors_units.drop(columns=['Score'])
+    investors_units = compute_affinities(investors_concepts, units_concepts, investors_units, concepts_concepts)
+
+    ############################################################
+
+    bc.log('Inserting investors-units edges into database...')
+
+    table_name = 'ca_temp.Edges_N_Investor_N_Unit_T_Years'
+    definition = [
+        'InvestorID CHAR(64)',
+        'Year SMALLINT',
+        'UnitID CHAR(32)',
+        'Score FLOAT',
+        'KEY InvestorID (InvestorID)',
+        'KEY Year (Year)',
+        'KEY UnitID (UnitID)'
+    ]
+    db.drop_create_insert_table(table_name, definition, investors_units)
 
     ############################################################
 
