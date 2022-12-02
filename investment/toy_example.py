9964ef776e04df31b421b18f82cb45b833cb8eda
Aitor Pérez <aitor.perez@epfl.ch>
2022-12-02 17:49:58 +0100

    feat: arrange toy examples as functions

--- investment/toy_example.py
+++ investment/toy_example.py
@@ -27,27 +27,90 @@ from investment.compute_investors_units import *
 #     'Score': [0.7, 0.6, 0.2, 0.9]
 # })
 
-
-
 ###############################################################
 
-x = pd.DataFrame({
-    'KeyX': ['A', 'A', 'B', 'B', 'B', 'C', 'C'],
-    'PageID': [1, 2, 1, 2, 3, 2, 3],
-    'Score': [0.7, 0.2, 0.4, 0.5, 0.5, 0.6, 0.9]
-})
 
-y = pd.DataFrame({
-    'KeyY': ['P', 'P', 'P', 'Q', 'Q'],
-    'PageID': [1, 2, 3, 3, 4],
-    'Score': [0.2, 0.1, 0.3, 0.3, 0.5]
-})
+def base():
+    x = pd.DataFrame({
+        'KeyX': ['A', 'A', 'B', 'B', 'B', 'C', 'C'],
+        'PageID': [1, 2, 1, 2, 3, 2, 3],
+        'Score': [0.7, 0.2, 0.4, 0.5, 0.5, 0.6, 0.9]
+    })
 
-edges = pd.DataFrame({
-    'SourcePageID': [1, 1, 3],
-    'TargetPageID': [2, 3, 4],
-    'Score': [1, 1, 1]
-})
+    y = pd.DataFrame({
+        'KeyY': ['P', 'P', 'P', 'Q', 'Q'],
+        'PageID': [1, 2, 3, 3, 4],
+        'Score': [0.2, 0.1, 0.3, 0.3, 0.5]
+    })
+
+    edges = pd.DataFrame({
+        'SourcePageID': [1, 1, 3],
+        'TargetPageID': [2, 3, 4],
+        'Score': [1, 1, 1]
+    })
+
+    return x, y, edges
+
+
+def path():
+    edges = pd.DataFrame({
+        'SourcePageID': [1, 2, 3, 4],
+        'TargetPageID': [2, 3, 4, 5],
+        'Score': [1, 1, 1, 1]
+    })
+
+    x = pd.DataFrame({
+        'KeyX': ['A', 'B', 'C', 'D', 'E'],
+        'PageID': [1, 2, 3, 4, 5],
+        'Score': [1, 1, 1, 1, 1]
+    })
+
+    y = x.rename(columns={'KeyX': 'KeyY'})
+
+    return x, y, edges
+
+
+def complete():
+    edges = pd.DataFrame({
+        'SourcePageID': [1, 1, 1, 1, 2, 2, 2, 3, 3, 4],
+        'TargetPageID': [2, 3, 4, 5, 3, 4, 5, 4, 5, 5],
+        'Score': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    })
+
+    x = pd.DataFrame({
+        'KeyX': ['A', 'B', 'C', 'C'],
+        'PageID': [1, 2, 1, 2],
+        'Score': [1, 1, 0.5, 0.5]
+    })
+
+    y = x.rename(columns={'KeyX': 'KeyY'})
+
+    return x, y, edges
+
+
+def random(n):
+    np.random.seed(0)
+
+    edges = pd.merge(
+        pd.DataFrame({'SourcePageID': range(n)}),
+        pd.DataFrame({'TargetPageID': range(n)}),
+        how='cross'
+    )
+    edges['Score'] = np.power(np.random.rand(len(edges)), 4)
+
+    m = 3
+    x = pd.DataFrame({
+        'KeyX': ['A'] * m,
+        'PageID': range(m),
+        'Score': np.random.rand(m)
+    })
+
+    y = x.rename(columns={'KeyX': 'KeyY'})
+
+    return x, y, edges
+
+
+x, y, edges = random(100)
 
 pairs = pd.merge(
     x['KeyX'].drop_duplicates(),
@@ -55,5 +118,7 @@ pairs = pd.merge(
     how='cross'
 )
 
+
+mix(x, normalise_graph(edges))
 
 print(compute_affinities(x, y, pairs, edges=edges, mix_x=True, mix_y=True))
