5662e80bc068cc03bf42038ac708218e3d5371fe
Aitor Pérez <aitor.perez@epfl.ch>
2022-12-13 13:51:46 +0100

    minor: add script to visualise scores combination manual-nlp

---
+++ graph/explore_scores_2.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+manual_scores = pd.Series(np.linspace(0, 1, 51), name='x')
+nlp_scores = pd.Series(np.linspace(0, 1, 51), name='y')
+
+df = pd.merge(manual_scores, nlp_scores, how='cross')
+
+df['b'] = pd.concat([df['x'], 0.9 * df['x'] + 0.1 * df['y']], axis=1).max(axis=1)
+
+df['score'] = np.power(df['b'], 1 - df['y'] / 2)
+
+df['score'] = df['score'].mask((df['score'] < 0.1) & (df['x'] == 0), 0)
+
+df = df.pivot(index='x', columns='y', values='score')
+
+sns.heatmap(df)
+plt.show()
