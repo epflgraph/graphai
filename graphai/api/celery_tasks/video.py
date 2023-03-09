54f3dec0875a1a7576879268da18245b89f95f01
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2023-03-09 13:40:29 +0100

    add pure celery version of dummy video task

--- graphai/api/celery_tasks/video.py
+++ graphai/api/celery_tasks/video.py
@@ -1,4 +1,4 @@
-from celery import shared_task
+from celery import shared_task, chain, group, chord
 import ray
 import time
 from graphai.api.common.log import log
@@ -34,3 +34,24 @@ def multiproc_example_task(self, data):
     log(f'Finished all tasks', sw.total(), total=True)
 
     return {'baz': baz}
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 1},
+             name='video.some_dummy', ignore_result=False)
+def do_something_celery(self, x):
+    time.sleep(x)
+    return True
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 1},
+             name='video.some_post_dummy', ignore_result=False)
+def do_some_callback_celery(self, l):
+    return all(l)
+
+def celery_multiproc_example_task(data):
+    sw = Stopwatch()
+    foo = data.foo
+    bar = data.bar
+    log(f'Got input parameters ({foo}, {bar})', sw.delta())
+    t = chord(do_something_celery.s(foo) for i in range(bar))(do_some_callback_celery.s()).get()
+    log(f'Got all results', sw.delta())
+    print(t)
+    return {'baz': t}
--- graphai/api/routers/video.py
+++ graphai/api/routers/video.py
@@ -13,8 +13,12 @@ router = APIRouter(
 )
 
 
-@router.post('/multiprocessing_example', response_model=TaskIDResponse)
+@router.post('/multiprocessing_example', response_model=MultiprocessingExampleResponse)
 async def multiprocessing_example(data: MultiprocessingExampleRequest):
-    task = multiproc_example_task.apply_async(args=[data])
-    return JSONResponse({"task_id": task.id})
+    result = multiproc_example_task.apply_async(args=[data]).get()
+    return result
 
+@router.post('/multiprocessing_example_purecelery', response_model=MultiprocessingExampleResponse)
+async def multiprocessing_example(data: MultiprocessingExampleRequest):
+    result = celery_multiproc_example_task(data)
+    return result
