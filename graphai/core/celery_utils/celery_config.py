aae8d4fc57c48b435effece7080ff27aebf071df
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2023-03-09 11:34:37 +0100

    add flag to celery to allow ray to run alongside it

--- graphai/core/celery_utils/celery_config.py
+++ graphai/core/celery_utils/celery_config.py
@@ -15,6 +15,7 @@ def route_task(name, args, kwargs, options, task=None, **kw):
 class BaseConfig:
     CELERY_BROKER_URL: str = os.environ.get("CELERY_BROKER_URL", "amqp://guest:guest@localhost:5672//")
     CELERY_RESULT_BACKEND: str = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
+    CELERY_WORKER_REDIRECT_STDOUTS: bool = False
 
     CELERY_TASK_QUEUES: list = (
         # default queue
