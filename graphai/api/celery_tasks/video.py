c9d731452e58ebbd35505f30de30f4d22837ca5e
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2023-03-09 16:06:48 +0100

    switch to group + chain instead of chord

--- graphai/api/celery_tasks/video.py
+++ graphai/api/celery_tasks/video.py
@@ -1,4 +1,4 @@
-from celery import shared_task, chain, group, chord
+from celery import shared_task, chord, group
 import ray
 import time
 from graphai.api.common.log import log
@@ -6,41 +6,15 @@ from graphai.core.utils.time.stopwatch import Stopwatch
 
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 1},
-             name='video.multiprocessing_example', ignore_result=False)
-def multiproc_example_task(self, data, video_actor_list):
-    # Initialize stopwatch to track time
-    sw = Stopwatch()
-
-    # Get input parameters
-    foo = data.foo
-    bar = data.bar
-    log(f'Got input parameters ({foo}, {bar})', sw.delta())
-
-    # Execute tasks in parallel
-    results = [video_actor_list.get_actor(i).do_something.remote(foo) for i in range(bar)]
-    log(f'Dispatched all tasks in parallel to the actors', sw.delta())
-
-    # Wait for the results
-    results = ray.get(results)
-    log(f'Got all results', sw.delta())
-
-    # Combine the results
-    baz = all(results)
-    log(f'Finished all tasks', sw.total(), total=True)
-
-    return {'baz': baz}
-
-
-@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 1},
              name='video.some_dummy', ignore_result=False)
-def do_something_celery(self, x):
+def do_something_in_parallel(self, x):
     time.sleep(x)
     return True
 
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 1},
              name='video.some_post_dummy', ignore_result=False)
-def do_some_callback_celery(self, l):
+def do_some_callback(self, l):
     return all(l)
 
 
@@ -49,6 +23,7 @@ def celery_multiproc_example_task(data):
     foo = data.foo
     bar = data.bar
     log(f'Got input parameters ({foo}, {bar})', sw.delta())
-    t = chord(do_something_celery.s(foo) for i in range(bar))(do_some_callback_celery.s()).get()
+    t = (group(do_something_in_parallel.signature(args=[foo], priority=4) for i in range(bar)) |
+                        do_some_callback.signature(args=[], priority=4)).apply_async().get()
     log(f'Got all results', sw.delta())
     return {'baz': t}
--- graphai/api/routers/video.py
+++ graphai/api/routers/video.py
@@ -1,12 +1,8 @@
-import time
-
 from fastapi import APIRouter
-
-import ray
 
 from graphai.api.schemas.video import *
 
-from graphai.api.celery_tasks.video import multiproc_example_task, celery_multiproc_example_task
+from graphai.api.celery_tasks.video import celery_multiproc_example_task
 
 
 # Initialise video router
@@ -17,47 +13,7 @@ router = APIRouter(
 )
 
 
-# Define ray actor
-@ray.remote
-class VideoActor:
-    """
-    Class representing a ray Actor to perform video tasks in parallel.
-    """
-
-    def do_something(self, x):
-        # Perform some time-consuming task
-        time.sleep(x)
-
-        return True
-
-
-class VideoActorList:
-
-    def __init__(self, n):
-        self.n = n
-        self.actors = []
-
-    def instantiate_actors(self):
-        self.actors = [VideoActor.remote() for i in range(self.n)]
-
-    def free_actors(self):
-        self.actors = []
-
-    def get_actor(self, i):
-        return self.actors[i % self.n]
-
-
-# Instantiate ray actor list
-video_actor_list = VideoActorList(16)
-
-
-# @router.post('/multiprocessing_example', response_model=MultiprocessingExampleResponse)
-# async def multiprocessing_example(data: MultiprocessingExampleRequest):
-#     result = multiproc_example_task.apply_async(args=[data, video_actor_list]).get()
-#     return result
-
-
-@router.post('/multiprocessing_example_purecelery', response_model=MultiprocessingExampleResponse)
+@router.post('/multiprocessing_example', response_model=MultiprocessingExampleResponse)
 async def multiprocessing_example(data: MultiprocessingExampleRequest):
     result = celery_multiproc_example_task(data)
     return result
