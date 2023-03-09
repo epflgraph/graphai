d539dfda8c5d4ac2c8ad5a81c36027cd35a88324
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2023-03-09 17:06:41 +0100

    add dict key to result

--- graphai/api/celery_tasks/ontology.py
+++ graphai/api/celery_tasks/ontology.py
@@ -2,9 +2,9 @@ from celery import shared_task
 
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 5},
-             name='ontology.get_ontology_tree_task', ignore_result=False)
+             name='ontology.tree', ignore_result=False)
 def get_ontology_tree_task(self, ontology):
-    return ontology.categories_categories.to_dict(orient='records')
+    return {'child_to_parent': ontology.categories_categories.to_dict(orient='records')}
 
 
 @shared_task(bind=True,autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 5},
