9fb5a2c25d37d40464351a51ed58e224896e5473
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2023-03-15 13:25:01 +0100

    add function to download file from url

--- graphai/core/common/video.py
+++ graphai/core/common/video.py
@@ -1,13 +1,19 @@
 import os
 import time
 import xml.etree.cElementTree as ET
+import wget
 import re
 import ffmpeg
 
 ROOT_VIDEO_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), '../../../Storage/'))
-
-
 STANDARD_FPS = 30
+
+
+def retrieve_file_from_url(url, out_filename=None):
+    if out_filename is None:
+        out_filename = url.split('/')[-1]
+    response = wget.download(url, out_filename)
+    return response
 
 
 def hash_video_or_audio(input_filename, video=True):
@@ -19,7 +25,7 @@ def hash_video_or_audio(input_filename, video=True):
         # audio
         in_stream = in_stream.audio
     result, _ = ffmpeg.output(in_stream, 'pipe:', format='md5').run(capture_stdout=True)
-    # The result looks like 'MD5=whateverthehashresultsare\n' so we clean it up
+    # The result looks like 'MD5=9735151f36a3e628b0816b1bba3b9640\n' so we clean it up
     return (result.decode('utf8').strip())[4:]
 
 
