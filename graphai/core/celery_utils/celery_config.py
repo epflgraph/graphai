c4adcf293cc35f690f5926f4eb69d3a32d785c81
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2023-04-05 11:24:14 +0200

    load celery broker and result backend urls from config file in CONFIG_DIR

--- graphai/core/celery_utils/celery_config.py
+++ graphai/core/celery_utils/celery_config.py
@@ -1,7 +1,11 @@
 import os
 from functools import lru_cache
 from kombu import Queue
+import configparser
+from graphai.definitions import CONFIG_DIR
 
+DEFAULT_BROKER = "amqp://guest:guest@localhost:5672//"
+DEFAULT_CACHE = "redis://localhost:6379/0"
 
 def route_task(name, args, kwargs, options, task=None, **kw):
     if "." in name:
@@ -11,20 +15,31 @@ def route_task(name, args, kwargs, options, task=None, **kw):
 
 
 class BaseConfig:
-    broker_url: str = os.environ.get("CELERY_BROKER_URL", "amqp://guest:guest@localhost:5672//")
-    result_backend: str = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
-    CELERY_WORKER_REDIRECT_STDOUTS: bool = False
 
-    CELERY_TASK_QUEUES: list = (
-        # default queue
-        Queue("celery"),
-        # custom queue
-        Queue("text", max_priority=10),
-        Queue("video", max_priority=2),
-        Queue("ontology", max_priority=6)
-    )
+    def __init__(self):
+        db_config = configparser.ConfigParser()
+        try:
+            print('Reading celery configuration from file')
+            db_config.read(f'{CONFIG_DIR}/celery.ini')
+            self.broker_url = db_config['CELERY'].get('broker_url', fallback=DEFAULT_BROKER)
+            self.result_backend = db_config['CELERY'].get('result_backend', fallback=DEFAULT_CACHE)
+        except Exception as e:
+            print(f'Could not read file {CONFIG_DIR}/celery.ini or '
+                  f'file does not have section [CELERY], falling back to defaults.')
+            self.broker_url = DEFAULT_BROKER
+            self.result_backend = DEFAULT_CACHE
 
-    CELERY_TASK_ROUTES = (route_task,)
+        self.CELERY_WORKER_REDIRECT_STDOUTS: bool = False
+        self.CELERY_TASK_QUEUES: list = [
+            # default queue
+            Queue("celery"),
+            # custom queue
+            Queue("text", max_priority=10),
+            Queue("video", max_priority=2),
+            Queue("ontology", max_priority=6)
+        ]
+
+        self.CELERY_TASK_ROUTES = (route_task,)
 
 
 class DevelopmentConfig(BaseConfig):
