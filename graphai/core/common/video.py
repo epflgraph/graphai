8ef4b90fe576b6776f41084b822e97a99f3a0b29
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2023-04-25 12:28:29 +0200

    remove deprecated google cloud storage functions

--- graphai/core/common/video.py
+++ graphai/core/common/video.py
@@ -407,39 +407,6 @@ def find_closest_image_fingerprint_from_list(target_fp, fp_list, token_list, min
                                               decoder_func=imagehash.hex_to_hash)
 
 
-def generate_gcp_uri(bucket_name, blob_name):
-    return f'gs://{bucket_name}/{blob_name}'
-
-
-def upload_file_to_google_cloud(input_filename_with_path, input_token, bucket_name='epflgraph_bucket'):
-    """Uploads a file to the bucket."""
-    # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
-    # The path to your file to upload
-    # source_file_name = "local/path/to/file"
-    # The ID of your GCS object
-    # destination_blob_name = "storage-object-name"
-
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(input_token)
-
-    # Optional: set a generation-match precondition to avoid potential race conditions
-    # and data corruptions. The request to upload is aborted if the object's
-    # generation number does not match your precondition. For a destination
-    # object that does not yet exist, set the if_generation_match precondition to 0.
-    # If the destination object already exists in your bucket, set instead a
-    # generation-match precondition using its generation number.
-    generation_match_precondition = 0
-
-    blob.upload_from_filename(input_filename_with_path, if_generation_match=generation_match_precondition)
-
-    print(
-        f"File {input_filename_with_path} uploaded to {generate_gcp_uri(bucket_name, input_token)}."
-    )
-    return True
-
-
 def extract_frames(input_filename_with_path, output_folder_with_path, output_folder):
     """
     Extracts frames from an image file
