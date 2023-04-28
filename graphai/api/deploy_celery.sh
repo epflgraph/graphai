e2f22879609b2c28ff739add21d2e363c6ff8580
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2023-04-28 13:39:04 +0200

    use two celery workers with different settings for different queues

--- graphai/api/deploy_celery.sh
+++ graphai/api/deploy_celery.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-celery -A main.celery_instance worker --pool threads --loglevel=debug -Q common,text,video,ontology
+celery -A main.celery_instance worker --hostname w1@%h -l debug -P threads --prefetch-multiplier 1 -c 16 -Q common,video,ontology -D
+celery -A main.celery_instance worker --hostname w2@%h -l debug -P prefork --prefetch-multiplier 4 -c 16 -Q text
