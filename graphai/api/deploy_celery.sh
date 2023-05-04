b088c0d92dd4654f5f969b05f89df71803e05048
Aitor Pérez <aitor.perez@epfl.ch>
2023-05-04 16:58:52 +0200

    :construction_worker: add flower deployment to deploy_celery

--- graphai/api/deploy_celery.sh
+++ graphai/api/deploy_celery.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 nice -n 20 celery -A main.celery_instance worker --hostname w1@%h -l debug -P threads --prefetch-multiplier 1 -c 16 -Q celery,video,ontology -D
-nice -n 0 celery -A main.celery_instance worker --hostname w2@%h -l debug -P threads --prefetch-multiplier 20 -c 16 -Q text
+nice -n 0 celery -A main.celery_instance worker --hostname w2@%h -l debug -P threads --prefetch-multiplier 20 -c 16 -Q text -D
+celery -A main.celery_instance flower --port=5555
