8bdb7084c47d9bce61f84461385d06614710b430
Aitor Pérez <aitor.perez@epfl.ch>
2023-05-04 13:51:20 +0200

    :wrench: update celery deployment to threads for text and set prefetch-multiplier to 20

--- graphai/api/deploy_celery.sh
+++ graphai/api/deploy_celery.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 nice -n 20 celery -A main.celery_instance worker --hostname w1@%h -l debug -P threads --prefetch-multiplier 1 -c 16 -Q celery,video,ontology -D
-nice -n 0 celery -A main.celery_instance worker --hostname w2@%h -l debug -P threads --prefetch-multiplier 4 -c 16 -Q text
+nice -n 0 celery -A main.celery_instance worker --hostname w2@%h -l debug -P threads --prefetch-multiplier 20 -c 16 -Q text
