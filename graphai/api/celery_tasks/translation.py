f549ae9d9e9f263478f52f5302e7471d7383f8cf
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2023-05-17 12:28:00 +0200

    add caching and fingerprinting to translate endpoint

--- graphai/api/celery_tasks/translation.py
+++ graphai/api/celery_tasks/translation.py
@@ -1,32 +1,158 @@
 from celery import shared_task
+from datetime import datetime
 from graphai.api.common.video import translation_models
-from graphai.core.common.video import detect_text_language
+from graphai.core.common.video import detect_text_language, perceptual_hash_text
+from graphai.core.common.caching import TextDBCachingManager
+from graphai.api.celery_tasks.common import fingerprint_lookup_retrieve_from_db, \
+    fingerprint_lookup_parallel, fingerprint_lookup_callback
 
 LONG_TEXT_ERROR = "Unpunctuated text too long (over 512 tokens), " \
                   "try adding punctuation or providing a smaller chunk of text."
 
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
+             name='text_6.fingerprint_text', ignore_result=False)
+def compute_text_fingerprint_task(self, token, text, force=False):
+    db_manager = TextDBCachingManager()
+    existing = db_manager.get_details(token, cols=['fingerprint'])[0]
+    if existing is not None and not force:
+        if existing['fingerprint'] is not None:
+            return {
+                'result': existing['fingerprint'],
+                'fresh': False
+            }
+    fp = perceptual_hash_text(text)
+    return {
+        'result': fp,
+        'fresh': True
+    }
+
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
+             name='text_6.fingerprint_text_callback', ignore_result=False)
+def compute_text_fingerprint_callback_task(self, results, token):
+    if results['fresh']:
+        db_manager = TextDBCachingManager()
+        db_manager.insert_or_update_details(
+            token,
+            {
+                'fingerprint': results['result']
+            }
+        )
+    return results
+
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
+             name='text_6.text_fingerprint_find_closest_retrieve_from_db', ignore_result=False)
+def text_fingerprint_find_closest_retrieve_from_db_task(self, results, token, equality_conditions):
+    db_manager = TextDBCachingManager()
+    return fingerprint_lookup_retrieve_from_db(results, token, db_manager, equality_conditions=equality_conditions)
+
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
+             name='text_6.text_fingerprint_find_closest_parallel', ignore_result=False)
+def text_fingerprint_find_closest_parallel_task(self, input_dict, token, i, n_total, equality_conditions,
+                                                min_similarity=1):
+    db_manager = TextDBCachingManager()
+    return fingerprint_lookup_parallel(input_dict, token, i, n_total, min_similarity, db_manager, data_type='text',
+                                       equality_conditions=equality_conditions)
+
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
+             name='text_6.text_fingerprint_find_closest_callback', ignore_result=False)
+def text_fingerprint_find_closest_callback_task(self, results_list, original_token):
+    db_manager = TextDBCachingManager()
+    return fingerprint_lookup_callback(results_list, original_token, db_manager)
+
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
+             name='text_6.retrieve_text_fingerprint_final_callback', ignore_result=False)
+def retrieve_text_fingerprint_callback_task(self, results):
+    # Returning the fingerprinting results, which is the part of this task whose results are sent back to the user.
+    results_to_return = results['fp_results']
+    results_to_return['closest'] = results['closest']
+    db_manager = TextDBCachingManager()
+    if results_to_return['closest'] is not None:
+        results_to_return['closest_origin'] = db_manager.get_origin(results_to_return['closest'])
+    else:
+        results_to_return['closest_origin'] = None
+    return results_to_return
+
+
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
              name='text_6.translate_text', translation_obj=translation_models, ignore_result=False)
-def translate_text_task(self, text, src, tgt):
+def translate_text_task(self, token, text, src, tgt, force):
     if src == tgt:
         return {
             'result': "'source' and 'target' languages must be different!",
             'text_too_large': False,
-            'successful': False
+            'successful': False,
+            'fresh': False
         }
+    db_manager = TextDBCachingManager()
+    if not force:
+        existing_list = db_manager.get_details(token, ['target'], using_most_similar=True)
+        # Unlike with audio and image, the token may not already exist in the table when this task is invoked.
+        #  Therefore, the task doesn't fail if the token doesn't exist.
+        for existing in existing_list:
+            if existing is None:
+                continue
+            if existing['target'] is not None:
+                print('Returning cached result')
+                return {
+                    'result': existing['target'],
+                    'text_too_large': False,
+                    'successful': True,
+                    'fresh': False
+                }
     how = f"{src}-{tgt}"
-    translated_text, large_warning = self.translation_obj.translate(text, how=how)
-    if translated_text is not None:
-        success = True
-    else:
-        success = False
+    try:
+        translated_text, large_warning = self.translation_obj.translate(text, how=how)
+        if translated_text is not None:
+            success = True
+        else:
+            success = False
         if large_warning:
             translated_text = LONG_TEXT_ERROR
+    except NotImplementedError as e:
+        print(e)
+        translated_text = str(e)
+        success = False
+        large_warning = False
+
     return {
         'result': translated_text,
         'text_too_large': large_warning,
-        'successful': success
+        'successful': success,
+        'fresh': success
     }
+
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
+             name='text_6.translate_text_callback', translation_obj=translation_models, ignore_result=False)
+def translate_text_callback_task(self, results, token, text, src, tgt):
+    if results['fresh']:
+        db_manager = TextDBCachingManager()
+        current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        values_dict = {
+            'source': text,
+            'target': results['result'],
+            'source_lang': src,
+            'target_lang': tgt,
+            'date_added': current_datetime
+        }
+        # Inserting values for original token
+        db_manager.insert_or_update_details(
+            token, values_dict
+        )
+        # Inserting the same values for closest token if different than original token
+        closest = db_manager.get_closest_match(token)
+        if closest is not None and closest != token:
+            db_manager.insert_or_update_details(
+                closest, values_dict
+            )
+    return results
+
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
              name='text_6.detect_text_language', ignore_result=False)
--- graphai/api/routers/translation.py
+++ graphai/api/routers/translation.py
@@ -1,10 +1,15 @@
+from celery import group, chain
 from fastapi import APIRouter
-from ..schemas.common import TaskIDResponse
-from ..schemas.translation import TranslationRequest, TranslationResponse, \
+from graphai.api.schemas.common import TaskIDResponse
+from graphai.api.schemas.translation import TranslationRequest, TranslationResponse, \
     TextDetectLanguageRequest, TextDetectLanguageResponse
-from ..celery_tasks.translation import translate_text_task, detect_text_language_task
+from graphai.api.celery_tasks.translation import translate_text_task, translate_text_callback_task, \
+    detect_text_language_task, compute_text_fingerprint_task, compute_text_fingerprint_callback_task, \
+    text_fingerprint_find_closest_retrieve_from_db_task, text_fingerprint_find_closest_parallel_task, \
+    text_fingerprint_find_closest_callback_task, retrieve_text_fingerprint_callback_task
 from graphai.core.interfaces.celery_config import get_task_info
-from ..celery_tasks.common import format_api_results
+from graphai.api.celery_tasks.common import format_api_results, ignore_fingerprint_results_callback_task
+from graphai.core.common.video import md5_text
 
 
 router = APIRouter(
@@ -14,12 +19,48 @@ router = APIRouter(
 )
 
 
+def generate_src_tgt_dict(src, tgt):
+    return {'source_lang': src, 'target_lang': tgt}
+
+
+def generate_text_token(s, src, tgt):
+    return md5_text(s) + '_' + src + '_' + tgt
+
+
+def get_text_fingerprint_chain_list(token, text, src, tgt, force, min_similarity=0.99, n_jobs=8,
+                                     ignore_fp_results=False, results_to_return=None):
+    equality_conditions = generate_src_tgt_dict(src, tgt)
+    task_list = [
+        compute_text_fingerprint_task.s(token, text, force),
+        compute_text_fingerprint_callback_task.s(token),
+        text_fingerprint_find_closest_retrieve_from_db_task.s(token, equality_conditions),
+        group(text_fingerprint_find_closest_parallel_task.s(token, i, n_jobs, equality_conditions, min_similarity)
+              for i in range(n_jobs)),
+        text_fingerprint_find_closest_callback_task.s(token)
+    ]
+    if ignore_fp_results:
+        task_list += [ignore_fingerprint_results_callback_task.s(results_to_return)]
+    else:
+        task_list += [retrieve_text_fingerprint_callback_task.s()]
+    return task_list
+
+
 @router.post('/translate/', response_model=TaskIDResponse)
 async def translate(data: TranslationRequest):
     text = data.text
     src = data.source
     tgt = data.target
-    task = (translate_text_task.s(text, src, tgt)).apply_async(priority=6)
+    force = data.force
+    token = generate_text_token(text, src, tgt)
+    if not force:
+        task_list = get_text_fingerprint_chain_list(token, text, src, tgt, force,
+                                                    ignore_fp_results=True, results_to_return=token)
+        task_list += [translate_text_task.s(text, src, tgt, force)]
+    else:
+        task_list = [translate_text_task.s(token, text, src, tgt, force)]
+    task_list += [translate_text_callback_task.s(token, text, src, tgt)]
+    task = chain(task_list)
+    task = task.apply_async(priority=6)
     return {'task_id': task.id}
 
 
@@ -32,7 +73,8 @@ async def translate_status(task_id):
             task_results = {
                 'result': task_results['result'],
                 'text_too_large': task_results['text_too_large'],
-                'successful': task_results['successful']
+                'successful': task_results['successful'],
+                'fresh': task_results['fresh']
             }
         else:
             task_results = None
