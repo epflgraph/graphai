641fef2aee93ed13e21014937ebded688ed42236
Aitor Pérez <aitor.perez@epfl.ch>
2023-06-09 16:30:33 +0200

    :art: update compare scripts to compare rescoring

--- graphai/archive/abstracts/1_wikify.py
+++ graphai/archive/abstracts/1_wikify.py
@@ -3,11 +3,13 @@ import json
 
 from graphai.core.utils.text.io import read_json, save_json
 
-wikify_url = 'http://localhost:28800/text/wikify'
+wikify_url = 'http://localhost:28800/text/wikify?refresh_scores=false'
+
+prefix = 'no-rescore'
 
 names = ['wave-fields', 'schreier', 'collider', 'skills']
 
 for name in names:
     data = read_json(f'../../api/requests/0-simple/{name}.json')
     results = requests.post(wikify_url, data=json.dumps(data)).json()
-    save_json(results, f'patched-{name}.json')
+    save_json(results, f'{prefix}-{name}.json')
