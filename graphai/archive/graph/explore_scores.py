b087a2a202291eea6dff94c0c9b2294816880a01
Aitor Pérez <aitor.perez@epfl.ch>
2023-06-16 11:14:44 +0200

    :art: update script to plot scores

--- graphai/archive/graph/explore_scores.py
+++ graphai/archive/graph/explore_scores.py
@@ -10,17 +10,42 @@ df = pd.merge(values_in, values_out, how='cross')
 df['min_in_out'] = df.min(axis=1)
 df['max_in_out'] = df.max(axis=1)
 
-# df['r'] = (1 + df['min_in_out']) / (1 + df['max_in_out'])
-df['r'] = df['min_in_out'] / df['max_in_out']
 
-# df['score'] = np.tanh(0.5 * df['r'] * np.log(df['out']))
-# df['score'] = (np.power(df['out'], df['r']) - 1) / (np.power(df['out'], df['r']) + 1)
-# df['score'] = (np.power(df['out'], df['r']) - 1) / (np.power(df['out'], df['r']) + (100 / (df['out'] + df['in'])))
-# df['score'] = (np.power(df['out'], df['r']) - 1) / (np.power(df['out'], df['r']) + 1/np.power(df['max_in_out'], df['r']))
-# df['score'] = (np.power(df['out'], df['r']) - 1) / (np.power(df['out'], df['r']) + 100 * 1 / (np.power(df['in'], 1) + np.power(df['out'], 1)))
+def yves_score(df, x='out', y='in'):
+    r = df['min_in_out'] / df['max_in_out']
+    return r * df[y]
 
-df['score'] = 1 - 1 / (1 + np.log(1 + df['r'] * np.log(df['out'])))
-# df['score'] = df['r'] * df['in']
+
+def francisco_score(df, x='out', y='in'):
+    r = (1 + df['min_in_out']) / (1 + df['max_in_out'])
+    return 1 - 1 / (1 + np.log(1 + r * np.log(df[x])))
+
+
+def aitor_score(df, x='out', y='in'):
+    r = (1 + df['min_in_out']) / (1 + df['max_in_out'])
+    power = np.power(df[x], r)
+    return (power - 1) / (power + (100 / (df[x] + df[y])))
+
+
+def yves_score_sym(df):
+    s1 = yves_score(df, x='out', y='in')
+    s2 = yves_score(df, x='in', y='out')
+    return (s1 + s2) / 2
+
+
+def francisco_score_sym(df):
+    s1 = francisco_score(df, x='out', y='in')
+    s2 = francisco_score(df, x='in', y='out')
+    return (s1 + s2) / 2
+
+
+def aitor_score_sym(df):
+    s1 = aitor_score(df, x='out', y='in')
+    s2 = aitor_score(df, x='in', y='out')
+    return (s1 + s2) / 2
+
+
+df['score'] = aitor_score_sym(df)
 
 df = df.pivot(index='in', columns='out', values='score')
 
