31bf4caed39069fa8f01f1cf55b5a4a633737a6a
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2023-07-12 12:41:17 +0200

    add function to remove video, slide, and audio files related to old videos

--- graphai/scripts/video_processing/cleanup_cached_files.py
+++ graphai/scripts/video_processing/cleanup_cached_files.py
@@ -2,13 +2,15 @@ from graphai.core.common.caching import (
     VideoConfig,
     VideoDBCachingManager,
     AudioDBCachingManager,
-    SlideDBCachingManager
+    SlideDBCachingManager,
+    delete_file
 )
 from graphai.definitions import CONFIG_DIR
 from graphai.core.common.video import format_datetime_for_mysql
 
 from datetime import datetime, timedelta
 import configparser
+import numpy as np
 
 
 def get_cleanup_interval():
@@ -34,8 +36,47 @@ def get_oldest_acceptable_date(cleanup_interval):
 
 def find_old_videos(oldest_acceptable_date):
     db_manager = VideoDBCachingManager()
-    results = db_manager.get_all_details(['date_added'], latest_date=oldest_acceptable_date)
+    print('Finding old videos')
+    # We only get the rows whose origin_token is not null,
+    # because those are the ones that have not been cleaned up.
+    results = db_manager.get_all_details(['origin_token', 'date_added'], allow_nulls=False,
+                                         latest_date=oldest_acceptable_date)
     if results is None:
         return []
     id_list = list(results.keys())
     return id_list
+
+
+def remove_origin_urls(id_list):
+    video_db_manager = VideoDBCachingManager()
+    print('Erasing `origin_token` (URL) column for old videos')
+    for id_token in id_list:
+        video_db_manager.insert_or_update_details(id_token, values_to_insert={'origin_token': None})
+
+
+def remove_old_files(id_list):
+    storage_config = VideoConfig()
+    slide_db_manager = SlideDBCachingManager()
+    audio_db_manager = AudioDBCachingManager()
+    for id_token in id_list:
+        current_video_path = storage_config.generate_filepath(id_token)
+        print("Deleting video file")
+        delete_file(current_video_path)
+
+        print("Finding slide tokens")
+        slide_tokens = slide_db_manager.get_details_using_origin(id_token, [])
+        if slide_tokens is not None:
+            slide_tokens = [x['id_token'] for x in slide_tokens]
+            print("Deleting slide files")
+            for slide_token in slide_tokens:
+                current_slide_path = storage_config.generate_filepath(slide_token)
+                delete_file(current_slide_path)
+
+        print("Finding audio tokens")
+        audio_tokens = audio_db_manager.get_details_using_origin(id_token, [])
+        if audio_tokens is not None:
+            audio_token = audio_tokens[0]['id_token']
+            print("Deleting audio file")
+            current_audio_path = storage_config.generate_filepath(audio_token)
+            delete_file(current_audio_path)
+
