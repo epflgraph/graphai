63ebfd7f950858b44b16a5b98c28dd89774e4bb8
Aitor Pérez <aitor.perez@epfl.ch>
2023-07-20 15:20:08 +0200

    :bug: fix imports in main

--- graphai/pipelines/investment/main.py
+++ graphai/pipelines/investment/main.py
@@ -1,12 +1,12 @@
-from graphai.scripts.investment.create_investments_graph import create_investments_graph
-from graphai.scripts.investment.aggregate_recent_past import aggregate_recent_past
-from graphai.scripts.investment.compute_jaccard_index import compute_jaccard_index
-from graphai.scripts.investment.detect_fundraisers_concepts import detect_fundraisers_concepts
-from graphai.scripts.investment.compute_investors_units import compute_investors_units
-from graphai.scripts.investment.compute_fundraisers_units import compute_fundraisers_units
-from graphai.scripts.investment.compute_investors_units_2 import compute_investors_units_2
+from graphai.pipelines.investment.create_investments_graph import create_investments_graph
+from graphai.pipelines.investment.aggregate_recent_past import aggregate_recent_past
+from graphai.pipelines.investment.compute_jaccard_index import compute_jaccard_index
+from graphai.pipelines.investment.detect_fundraisers_concepts import detect_fundraisers_concepts
+from graphai.pipelines.investment.compute_investors_units import compute_investors_units
+from graphai.pipelines.investment.compute_fundraisers_units import compute_fundraisers_units
+from graphai.pipelines.investment.compute_investors_units_2 import compute_investors_units_2
 
-import graphai.scripts.investment.parameters as params
+import graphai.pipelines.investment.parameters as params
 
 from graphai.core.utils.breadcrumb import Breadcrumb
 
