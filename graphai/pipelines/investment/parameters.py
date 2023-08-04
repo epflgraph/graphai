1842561614b5bbff49ece42c905d49ce9c58d3a9
Aitor Pérez <aitor.perez@epfl.ch>
2023-08-04 14:46:37 +0200

    :recycle: make ALL_ALL_ALL the default parameter configuration

--- graphai/pipelines/investment/parameters.py
+++ graphai/pipelines/investment/parameters.py
@@ -31,24 +31,24 @@ recent_past = {
 switzerland = ['CHE']
 switzerland_nbh = ['CHE', 'DEU', 'FRA', 'ITA', 'AUT', 'BEL', 'NLD', 'LUX']
 
-countries = switzerland
+# countries = switzerland
 # countries = switzerland_nbh
-# countries = None
+countries = None
 
 ##################
 # Investor types #
 ##################
 # Only investors whose type is in the list will be considered, the rest will be ignored.
-investor_types = ['Person']
-# investor_types = ['Organization', 'Person']
+# investor_types = ['Person']
+investor_types = ['Organisation', 'Person']
 
 #######################
 # Funding round types #
 #######################
 # Only funding rounds whose type is in the list will be considered, the rest will be ignored.
 # None means no filtering is done and thus all funding rounds are considered.
-fr_types = ['pre seed']
-# fr_types = None
+# fr_types = ['pre seed']
+fr_types = None
 
 
 #################
