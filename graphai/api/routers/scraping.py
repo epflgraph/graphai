a5aebb98b822be1ee5e90e156b516127768e6cf3
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2023-09-08 14:20:43 +0200

    add /scraping/content endpoint

--- graphai/api/routers/scraping.py
+++ graphai/api/routers/scraping.py
@@ -5,18 +5,26 @@ from graphai.api.schemas.common import TaskIDResponse
 
 from graphai.api.schemas.scraping import (
     GetSublinksRequest,
-    GetSublinksResponse
+    GetSublinksResponse,
+    ExtractContentRequest,
+    ExtractContentResponse
 )
 
 from graphai.api.celery_tasks.common import (
     format_api_results,
     ignore_fingerprint_results_callback_task,
+    text_dummy_task
 )
 
 from graphai.api.celery_tasks.scraping import (
     initialize_scraping_url_task,
     get_scraping_sublinks_task,
-    scraping_sublinks_callback_task
+    scraping_sublinks_callback_task,
+    process_all_scraping_sublinks_preprocess_task,
+    process_all_scraping_sublinks_parallel_task,
+    process_all_scraping_sublinks_callback_task,
+    remove_junk_scraping_parallel_task,
+    extract_scraping_content_callback_task
 )
 
 from graphai.core.common.scraping import create_base_url_token
@@ -55,6 +63,52 @@ async def extract_sublinks_status(task_id):
                 'token': task_results['token'],
                 'validated_url': task_results['validated_url'],
                 'sublinks': task_results['sublinks'],
+                'status_msg': task_results['status_msg'],
+                'successful': task_results['sublinks'] is not None,
+                'fresh': task_results['fresh']
+            }
+        else:
+            task_results = None
+    return format_api_results(full_results['id'], full_results['name'], full_results['status'], task_results)
+
+
+@router.post('/content', response_model=TaskIDResponse)
+async def extract_page_content(data: ExtractContentRequest):
+    url = data.url
+    force = data.force
+    headers = data.headers
+    long_patterns = data.long_patterns
+    n_jobs = 8
+
+    token = create_base_url_token(url)
+    task_list = [
+        initialize_scraping_url_task.s(token, url, force),
+        get_scraping_sublinks_task.s(),
+        scraping_sublinks_callback_task.s(token),
+        process_all_scraping_sublinks_preprocess_task.s(token, force),
+        group(process_all_scraping_sublinks_parallel_task.s(i, n_jobs) for i in range(n_jobs)),
+        process_all_scraping_sublinks_callback_task.s(),
+        text_dummy_task.s(),
+        group(remove_junk_scraping_parallel_task.s(i, n_jobs, headers, long_patterns) for i in range(n_jobs)),
+        extract_scraping_content_callback_task.s()
+    ]
+    task = chain(task_list)
+    task = task.apply_async(priority=6)
+    return {'task_id': task.id}
+
+
+@router.get('/content/status/{task_id}', response_model=ExtractContentResponse)
+async def extract_page_content_status(task_id):
+    full_results = get_task_info(task_id)
+    task_results = full_results['results']
+    if task_results is not None:
+        if 'data' in task_results:
+            task_results = {
+                'token': task_results['token'],
+                'validated_url': task_results['validated_url'],
+                'sublinks': task_results['sublinks'],
+                'status_msg': task_results['status_msg'],
+                'data': task_results['data'],
                 'successful': task_results['sublinks'] is not None,
                 'fresh': task_results['fresh']
             }
