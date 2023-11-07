062393b6ebe97071cdf0c187bf101892d2cb879a
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2023-11-07 11:41:25 +0100

    add task for choosing best slide subset

--- graphai/api/celery_tasks/completion.py
+++ graphai/api/celery_tasks/completion.py
@@ -1,7 +1,7 @@
 import json
 
 from celery import shared_task
-from graphai.core.common.text_utils import force_dict_to_text, ChatGPTSummarizer
+from graphai.core.common.text_utils import force_dict_to_text, ChatGPTSummarizer, find_best_slide_subset
 from graphai.core.common.common_utils import get_current_datetime
 from graphai.core.common.caching import CompletionDBCachingManager
 from graphai.core.text.keywords import get_keywords
@@ -323,4 +323,17 @@ def simulate_cleanup_task(self, text, text_type='text', result_type='cleanup'):
         'too_many_tokens': too_many_tokens,
         'n_tokens_total': token_count,
         'full_message': system_message
+    }
+
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
+             name='text_6.choose_best_subset', ignore_result=False)
+def choose_best_subset_task(self, slide_number_to_concepts, coverage=1.0, min_freq=2):
+    slide_numbers = sorted(list(slide_number_to_concepts.keys()))
+    slide_concept_list = [slide_number_to_concepts[n]['concepts'] for n in slide_numbers]
+    # slide_raw_list = [slide_number_to_concepts[n].get('raw', None) for n in slide_numbers]
+    cover, best_indices = find_best_slide_subset(slide_concept_list, coverage, True, min_freq)
+    return {
+        'subset': [slide_numbers[i] for i in best_indices],
+        # 'raw': [slide_raw_list[i] for i in best_indices]
     }
