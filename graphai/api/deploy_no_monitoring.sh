dbb6a0bad7822694ba137de7d74b37d9c1cc3f89
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2023-11-15 11:56:54 +0100

    add no-monitoring deploy script

---
+++ graphai/api/deploy_no_monitoring.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+host="0.0.0.0"
+port=28800
+TIMEOUT=120
+
+while getopts ":h:p:" opt; do
+  case $opt in
+    h) host="$OPTARG"
+    ;;
+    p) port="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG. Usage: deploy.sh -h 0.0.0.0 -p 28800" >&2
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) echo "Option $opt needs a valid argument"
+    exit 1
+    ;;
+  esac
+done
+
+# uvicorn main:app --host $host --port $port
+gunicorn main:app -b $host:$port -w 1 -k uvicorn.workers.UvicornWorker --timeout $TIMEOUT
