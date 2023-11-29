d8b2457ca55dbff131181f2a3955cf6f1ae7b958
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2023-11-29 17:07:19 +0100

    add min_n to task

--- graphai/api/celery_tasks/ontology.py
+++ graphai/api/celery_tasks/ontology.py
@@ -27,7 +27,8 @@ def get_category_children_task(self, parent_id):
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 5},
              name='ontology_6.recompute_clusters', ignore_result=False, ontology_obj=ontology)
-def recompute_clusters_task(n_clusters):
+def recompute_clusters_task(self, n_clusters, min_n=None):
+    # TODO move the data loading step into a separate object
     db_manager = DB(load_db_config())
     concept_names = db_results_to_pandas_df(db_manager.execute_query(
         "SELECT id, name FROM graph_ontology.Nodes_N_Concept WHERE is_ontology_concept=1"),
@@ -47,10 +48,12 @@ def recompute_clusters_task(n_clusters):
             concept_names)
     )
     _, embedding = combine_and_embed_laplacian(list(graphs_dict.values()))
-    cluster_labels = cluster_and_reassign_outliers(embedding, n_clusters)
+    cluster_labels = cluster_and_reassign_outliers(embedding, n_clusters, min_n)
     unique_cluster_labels = sorted(list(set(cluster_labels.tolist())))
     result_dict = {
-        label: [concept_index_to_id[i] for i in np.where(cluster_labels == label)[0]]
+        label: [{'name': concept_index_to_name[i], 'id': concept_index_to_id[i]}
+                for i in np.where(cluster_labels == label)[0]]
         for label in unique_cluster_labels
     }
+    # TODO add evaluation to ensure that concepts aren't moved around between categories, or just leave it be?
     return result_dict
