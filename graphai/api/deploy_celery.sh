200d8efde06b6c27be552cc5bfddbf703ec602b0
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2024-01-31 15:15:13 +0100

    add new queue with prefork pool for scraping tasks

--- graphai/api/deploy_celery.sh
+++ graphai/api/deploy_celery.sh
@@ -2,4 +2,5 @@
 
 nice -n 0 celery -A main.celery_instance worker --hostname workerHigh@%h -l debug -P threads --prefetch-multiplier 20 -c 16 -Q text_10 -D
 nice -n 20 celery -A main.celery_instance worker --hostname workerLow@%h -l debug -P threads --prefetch-multiplier 1 -c 16 -Q celery,video_2,ontology_6,text_6 -D
+nice -n 20 celery -A main.celery_instance worker --hostname workerMid@%h -l debug -P prefork --prefetch-multiplier 20 -c 20 -Q scraping_6 -D
 FLOWER_UNAUTHENTICATED_API=true celery -A main.celery_instance flower --port=5555
