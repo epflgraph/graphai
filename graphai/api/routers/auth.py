8af39bce28801d2e712f50ab68d9b89b852ee0a2
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2024-02-19 11:37:23 +0100

    remove dummy endpoint

--- graphai/api/routers/auth.py
+++ graphai/api/routers/auth.py
@@ -109,10 +109,3 @@ async def read_users_me(
     current_user: Annotated[User, Depends(get_current_active_user)]
 ):
     return current_user
-
-
-@authenticated_router.get("/users/me/items/")
-async def read_own_items(
-    current_user: Annotated[User, Depends(get_current_active_user)]
-):
-    return [{"item_id": "Foo", "owner": current_user.username}]
