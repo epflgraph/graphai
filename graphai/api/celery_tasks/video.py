651ca460f9a539d03a706e6111e4a08280d30b6e
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2024-03-25 13:59:58 +0100

    add cache-based slide recalculation task

--- graphai/api/celery_tasks/video.py
+++ graphai/api/celery_tasks/video.py
@@ -628,6 +628,54 @@ def detect_slides_callback_task(self, results, token, force=False):
     }
 
 
+
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
+             name='video_2.reextract_cached_slides', ignore_result=False,
+             file_manager=file_management_config)
+def reextract_cached_slides_task(self, token):
+    video_db_manager = VideoDBCachingManager()
+    closest_token = video_db_manager.get_closest_match(token)
+    slide_db_manager = SlideDBCachingManager()
+    existing_slides_own = slide_db_manager.get_details_using_origin(token, cols=['slide_number', 'timestamp'])
+    if closest_token is not None and closest_token != token:
+        existing_slides_closest = slide_db_manager.get_details_using_origin(closest_token,
+                                                                            cols=['slide_number', 'timestamp'])
+    else:
+        existing_slides_closest = None
+    if existing_slides_own is None and existing_slides_closest is None:
+        return {
+            'slide_tokens': None,
+            'fresh': False
+        }
+    existing_slides = existing_slides_own if existing_slides_own is not None else existing_slides_closest
+    token_to_use_as_name = existing_slides[0]['origin_token']
+    timestamps_to_keep = sorted([x['timestamp'] for x in existing_slides])
+    output_folder = token_to_use_as_name + '_slides'
+    output_folder_with_path = self.file_manager.generate_filepath(output_folder)
+    input_filename_with_path = self.file_manager.generate_filepath(token)
+    output_folder = extract_frames(input_filename_with_path, output_folder_with_path, output_folder)
+    # If there was an error of any kind (e.g. non-existing video file), the returned token will be None
+    if output_folder is None:
+        return {
+            'slide_tokens': None,
+            'fresh': False
+        }
+    list_of_slides = [FRAME_FORMAT_PNG % x for x in timestamps_to_keep]
+    for f in os.listdir(output_folder_with_path):
+        if f not in list_of_slides:
+            os.remove(os.path.join(output_folder_with_path, f))
+    slide_tokens = [os.path.join(output_folder, s) for s in list_of_slides]
+    slide_tokens = {i + 1: {'token': slide_tokens[i], 'timestamp': timestamps_to_keep[i]}
+                    for i in range(len(slide_tokens))}
+    for slide_number in slide_tokens:
+        slide_tokens[slide_number]['token_status'] = get_image_token_status(slide_tokens[slide_number]['token'])
+    return {
+        'slide_tokens': slide_tokens,
+        'fresh': True
+    }
+
+
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
              name='video_2.init', ignore_result=False,
              transcription_obj=transcription_model,
