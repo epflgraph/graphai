ed47dad8b40c0978a75e1f263eaf6c5c9388627b
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2024-04-09 14:13:15 +0200

    add celery jobs for remaining ontology endpoints

--- graphai/api/celery_jobs/ontology.py
+++ graphai/api/celery_jobs/ontology.py
@@ -1,5 +1,3 @@
-from celery import chain
-
 from graphai.api.celery_tasks.ontology import (
     get_ontology_tree_task,
     get_category_info_task,
@@ -66,3 +64,53 @@ def recompute_clusters_job(n_clusters, min_n):
     task = recompute_clusters_task.s(n_clusters, min_n)
     task = task.apply_async(priority=6)
     return task.id
+
+
+def graph_distance_job(src, tgt, src_type, tgt_type, avg, coeffs):
+    assert coeffs is None or len(coeffs) == 2
+    if src_type == 'concept' and tgt_type == 'category':
+        task = get_concept_category_similarity_task.s(src, tgt, avg, coeffs)
+    elif src_type == 'category' and tgt_type == 'concept':
+        task = get_concept_category_similarity_task.s(tgt, src, avg, coeffs)
+    elif src_type == 'concept' and tgt_type == 'cluster':
+        task = get_concept_cluster_similarity_task.s(src, tgt, avg)
+    elif src_type == 'cluster' and tgt_type == 'concept':
+        task = get_concept_cluster_similarity_task.s(tgt, src, avg)
+    elif src_type == 'cluster' and tgt_type == 'cluster':
+        task = get_cluster_cluster_similarity_task.s(src, tgt, avg)
+    elif src_type == 'cluster' and tgt_type == 'category':
+        task = get_cluster_category_similarity_task.s(src, tgt, avg, coeffs)
+    elif src_type == 'category' and tgt_type == 'cluster':
+        task = get_cluster_category_similarity_task.s(tgt, src, avg, coeffs)
+    elif src_type == 'category' and tgt_type == 'category':
+        task = get_category_category_similarity_task.s(src, tgt, avg, coeffs)
+    else:
+        task = get_concept_concept_similarity_task.s(src, tgt)
+    res = task.apply_async(priority=6).get(timeout=30)
+    return res
+
+
+def concept_nearest_category_job(src, avg, coeffs, top_n, use_depth_3, return_clusters):
+    assert coeffs is None or len(coeffs) == 2
+    task = get_concept_category_closest_task.s(src, avg, coeffs, top_n, use_depth_3, return_clusters)
+    res = task.apply_async(priority=6).get(timeout=30)
+    return res
+
+
+def cluster_nearest_category_job(src, avg, coeffs, top_n, use_depth_3):
+    assert coeffs is None or len(coeffs) == 2
+    task = get_cluster_category_closest_task.s(src, avg, coeffs, top_n, use_depth_3)
+    res = task.apply_async(priority=6).get(timeout=30)
+    return res
+
+
+def concept_nearest_concept_job(src, top_n):
+    task = get_concept_concept_closest_task.s(src, top_n)
+    res = task.apply_async(priority=6).get(timeout=30)
+    return res
+
+
+def breakup_cluster_job(cluster_id, n_clusters):
+    task = break_up_cluster_task.s(cluster_id, n_clusters)
+    res = task.apply_async(priority=6).get(timeout=30)
+    return res
