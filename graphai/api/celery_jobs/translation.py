af29be401960de5131a40dfdc03c33d3cc922ddf
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2024-04-16 13:28:10 +0200

    remove 'results_to_return'

--- graphai/api/celery_jobs/translation.py
+++ graphai/api/celery_jobs/translation.py
@@ -29,33 +29,17 @@ from graphai.core.common.caching import FingerprintParameters
 
 
 def get_translation_text_fingerprint_chain_list(token, text, src, tgt, min_similarity=None, n_jobs=8,
-                                                ignore_fp_results=False, results_to_return=None):
-    # Loading min similarity parameter for text
-    if min_similarity is None:
-        fp_parameters = FingerprintParameters()
-        min_similarity = fp_parameters.get_min_sim_text()
-    print(min_similarity)
-    print(min_similarity == 1.0)
-
+                                                ignore_fp_results=False):
     # Generating the equality condition dictionary
     equality_conditions = generate_src_tgt_dict(src, tgt)
     # The tasks are fingerprinting and callback, then lookup. The lookup is only among cache rows that satisfy the
     # equality conditions (source and target languages).
     task_list = [
-        compute_translation_text_fingerprint_task.s(token, text),
+        compute_translation_text_fingerprint_task.s(text),
         compute_translation_text_fingerprint_callback_task.s(text, src, tgt)
     ]
-    if min_similarity == 1.0:
-        task_list += [translation_text_fingerprint_find_closest_direct_task.s(equality_conditions)]
-    else:
-        task_list += [
-            translation_text_fingerprint_find_closest_retrieve_from_db_task.s(equality_conditions),
-            group(translation_text_fingerprint_find_closest_parallel_task.s(i, n_jobs, equality_conditions, min_similarity)
-                  for i in range(n_jobs))
-        ]
-    task_list += [translation_text_fingerprint_find_closest_callback_task.s()]
     if ignore_fp_results:
-        task_list += [ignore_fingerprint_results_callback_task.s(results_to_return)]
+        task_list += [ignore_fingerprint_results_callback_task.s()]
     else:
         task_list += [translation_retrieve_text_fingerprint_callback_task.s()]
     return task_list
@@ -100,28 +84,17 @@ def translation_job(text, src, tgt, force):
         if direct_lookup_task_id is not None:
             return direct_lookup_task_id
 
-    ##########################
-    # Fingerprint cache lookup
-    ##########################
-    # Now we do a fingerprint lookup to see if we can skip the fingerprinting chain or if it needs to be included
+    ####################################
+    # Fingerprinting and fp-based lookup
+    ####################################
     direct_fingerprint_lookup_task_id = fingerprint_lookup_job(token)
-    if direct_fingerprint_lookup_task_id is None:
-        # If the result is None, it means that there is no cached fingerprint, so we need to do fingerprinting
-        # The first task, "translate_text_task", will only have 'src' and 'tgt' in its signature
-        # because the first argument (which is 'text') will come from the preceding task chain.
-        task_list = get_translation_text_fingerprint_chain_list(token, text, src, tgt,
-                                                                ignore_fp_results=True, results_to_return=text)
-        task_list += [translate_text_task.s(src, tgt)]
-    else:
-        # We end up here if the fingerprint results are already cached. Remember that setting force=True
-        # in the translation endpoint only forces a re-translation, NOT a re-fingerprinting!
-
-        # If "translate_text_task" is the first task in the chain, then it'll have the full signature.
-        task_list = [translate_text_task.s(text, src, tgt)]
-    #########################
-    # Rest of computation job
-    #########################
-    task_list += [
+    if direct_fingerprint_lookup_task_id is not None and not force:
+        return direct_fingerprint_lookup_task_id
+    #################
+    # Computation job
+    #################
+    task_list = [
+        translate_text_task.s(text, src, tgt),
         translate_text_callback_task.s(token, text, src, tgt, force, return_list)
     ]
     task = chain(task_list)
--- graphai/api/celery_tasks/common.py
+++ graphai/api/celery_tasks/common.py
@@ -338,14 +338,12 @@ def fingerprint_lookup_callback(results_list, db_manager):
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
              name='video_2.ignore_fingerprint_results_callback', ignore_result=False)
-def ignore_fingerprint_results_callback_task(self, results, results_to_return=None):
+def ignore_fingerprint_results_callback_task(self, results):
     # Ignoring the fingerprinting results and returning the results relevant to the task chain.
     # Used in tasks like transcription and OCR, where fingerprinting is performed before the task itself, but where
     # the results of the fingerprinting are not returned.
-    if results_to_return is None:
-        return results['fp_results']['original_results']
-    else:
-        return results_to_return
+
+    return results['fp_results']['original_results']
 
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
