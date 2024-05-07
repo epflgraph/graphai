3b55f5d9fe5ceccb31b25717b9a2402531f251bd
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2024-05-07 13:18:11 +0200

    add creation of rate-limit override table

--- queries/auth/init_auth_db.sql
+++ queries/auth/init_auth_db.sql
@@ -1,5 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS `auth_graphai` ;
 
+-- The table containing user information
 CREATE TABLE IF NOT EXISTS `auth_graphai`.`Users` (
   `username` varchar(255) NOT NULL,
   `full_name` varchar(255) NOT NULL,
@@ -30,3 +31,18 @@ VALUES
 'yourbcrypthashedpassword',
 0,
 'user,voice,video,translation,text,scraping,ontology,image,completion');
+
+
+-- The table containing user-specific rate-limit overrides
+CREATE TABLE IF NOT EXISTS `auth_graphai`.`User_Rate_Limits` (
+  `username` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `max_requests` int DEFAULT NULL,
+  `window` int DEFAULT NULL,
+  PRIMARY KEY (`username`),
+  KEY `path` (`path`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- In order to disable rate limiting for a user for a given endpoint group, insert the username and path with
+-- max_requests and window set to NULL. Doing this for every endpoint group disables rate limiting for that user
+-- entirely.
