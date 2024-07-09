4055d365a4dfc6f3c511240ea1d97fcef4625932
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2024-07-09 15:56:30 +0200

    test the text_too_large flag

--- tests/fixtures/embedding.py
+++ tests/fixtures/embedding.py
@@ -4,3 +4,21 @@ import pytest
 @pytest.fixture
 def example_word():
     return "sparrow"
+
+
+@pytest.fixture
+def very_long_text():
+    return """
+    That means, the position embedding layer of the transformers has 512 weights, but the sentence transformer will 
+    only use and was trained with the first 256 of them. Therefore, you should be careful with increasing the
+    value above 256. It will work from a technical perspective, but the position embedding weights (>256) are not
+    properly trained and can therefore mess up your results. That means, the position embedding layer of the 
+    transformers has 512 weights, but the sentence transformer will 
+    only use and was trained with the first 256 of them. Therefore, you should be careful with increasing the
+    value above 256. It will work from a technical perspective, but the position embedding weights (>256) are not
+    properly trained and can therefore mess up your results. That means, the position embedding layer of the 
+    transformers has 512 weights, but the sentence transformer will 
+    only use and was trained with the first 256 of them. Therefore, you should be careful with increasing the
+    value above 256. It will work from a technical perspective, but the position embedding weights (>256) are not
+    properly trained and can therefore mess up your results.
+    """
