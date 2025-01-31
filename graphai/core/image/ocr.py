fcc7ea5af12888d73701e0cacfd4c2ac6e4cb96f
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2025-01-31 10:32:13 +0100

    set default pdf tesseract language to eng+fra

--- graphai/core/image/ocr.py
+++ graphai/core/image/ocr.py
@@ -102,23 +102,20 @@ def get_ocr_colnames(method):
         return ['ocr_google_1_results', 'ocr_google_2_results']
 
 
-def perform_tesseract_ocr_on_pdf(pdf_path, language=None):
+def perform_tesseract_ocr_on_pdf(pdf_path):
     """
     Performs OCR using tesseract on a pdf file
     Args:
         pdf_path: Path to the PDF file
-        language: Language of the pdf file
 
     Returns:
         String containing the entire PDF file's extracted contents
     """
-    if language is None:
-        language = 'en'
     if not file_exists(pdf_path):
         print(f'Error: File {pdf_path} does not exist')
         return None
     pdf_imageset = pdf2image.convert_from_path(pdf_path)
     return '\n'.join(
-        pytesseract.image_to_string(img, lang={'en': 'eng', 'fr': 'fra'}[language])
+        pytesseract.image_to_string(img, lang='eng+fra')
         for img in pdf_imageset
     )
