70525829e9874b000ae0cbda58c77294be5c9d11
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2025-02-14 15:26:04 +0100

    add lex search task and job combining embedding and search

---
+++ graphai/celery/retrieval/jobs.py
@@ -0,0 +1,14 @@
+from celery import chain
+from graphai.celery.retrieval.tasks import retrieve_lex_task
+from graphai.celery.embedding.tasks import embed_text_task
+from graphai.core.retrieval.retrieval_settings import RETRIEVAL_PARAMS
+
+
+def retrieve_lex_job(text, lang_filter=None, limit=10):
+    task_list = [
+        embed_text_task.s(text, RETRIEVAL_PARAMS['lex']['model']),
+        retrieve_lex_task.s(text, lang_filter, limit)
+    ]
+    task = chain(task_list)
+    task = task.apply_async(priority=10)
+    return task.id
---
+++ graphai/celery/retrieval/tasks.py
@@ -0,0 +1,11 @@
+from celery import shared_task
+from graphai.core.retrieval.retrieval_utils import search_lex
+
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 2},
+             name='text_10.retrieve_lex', ignore_result=False)
+def retrieve_lex_task(self, embedding_results, text, lang_filter, limit=10):
+    return search_lex(text,
+                      embedding_results['result'] if embedding_results['successful'] else None,
+                      lang_filter,
+                      limit)
