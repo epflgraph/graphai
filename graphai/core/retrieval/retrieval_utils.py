84f7cb27e4d2f87e845335c7918d56e6596c0fd0
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2025-02-17 11:12:08 +0100

    do not return embeddings by default

--- graphai/core/retrieval/retrieval_utils.py
+++ graphai/core/retrieval/retrieval_utils.py
@@ -2,14 +2,15 @@ from graphai.core.common.config import config
 from graphai.core.retrieval.retrieval_settings import RETRIEVAL_PARAMS
 
 
-def search_lex(text, embedding=None, lang=None, limit=10):
+def search_lex(text, embedding=None, lang=None, limit=10, return_embeddings=False):
     try:
         lex_retriever = RETRIEVAL_PARAMS['lex']['retrieval_class'](
             config['elasticsearch'],
             index=config['elasticsearch'].get('lex_index', RETRIEVAL_PARAMS['lex']['default_index'])
         )
+        results = lex_retriever.search(text, embedding, lang, limit=limit, return_embeddings=return_embeddings)
         return {
-            'result': lex_retriever.search(text, embedding, lang, limit),
+            'result': results,
             'successful': True
         }
     except Exception as e:
