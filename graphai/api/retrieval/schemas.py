d2538cee89b45d648a64d017da9745a08449becb
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2025-03-04 10:42:56 +0100

    linter fix

--- graphai/api/retrieval/schemas.py
+++ graphai/api/retrieval/schemas.py
@@ -1,8 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import Union, Literal, List, Dict
 
-from graphai.api.common.schemas import TaskStatusResponse
-
 
 class RetrievalRequest(BaseModel):
     text: str = Field(
