ef90598fd1956c875ae6d62d0d7ab934eef09594
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2025-03-11 11:57:13 +0100

    add dependency on elasticsearch interface 0.8.0 for ESServiceDesk

--- graphai/core/retrieval/retrieval_settings.py
+++ graphai/core/retrieval/retrieval_settings.py
@@ -1,10 +1,15 @@
-from elasticsearch_interface.es import ESLex
+from elasticsearch_interface.es import ESLex, ESServiceDesk
 
 
 RETRIEVAL_PARAMS = {
     "lex": {
         "default_index": "ramtin_lex_index",
         "retrieval_class": ESLex,
+        "model": "all-MiniLM-L12-v2"
+    },
+    "servicedesk": {
+        "default_index": "ramtin_servicedesk_index",
+        "retrieval_class": ESServiceDesk,
         "model": "all-MiniLM-L12-v2"
     }
 }
