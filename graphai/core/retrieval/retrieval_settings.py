9da237eaf287026a63ca8888e98e9d7331225065
Ramtin Yazdanian <ramtin.yazdanian@gmail.com>
2025-03-17 11:51:08 +0100

    provide fallback in case of outdated elasticsearch-interface version

--- graphai/core/retrieval/retrieval_settings.py
+++ graphai/core/retrieval/retrieval_settings.py
@@ -1,15 +1,19 @@
-from elasticsearch_interface.es import ESLex, ESServiceDesk
+from elasticsearch_interface.es import ESLex
+try:
+    from elasticsearch_interface.es import ESServiceDesk
+except ImportError:
+    print('Outdated elasticsearch-interface version, reverting to default ES retriever class')
+    ESServiceDesk = ESLex
 
+RETRIEVAL_PARAMS = dict()
+RETRIEVAL_PARAMS["lex"] = {
+    "default_index": "ramtin_lex_index",
+    "retrieval_class": ESLex,
+    "model": "all-MiniLM-L12-v2"
+}
 
-RETRIEVAL_PARAMS = {
-    "lex": {
-        "default_index": "ramtin_lex_index",
-        "retrieval_class": ESLex,
-        "model": "all-MiniLM-L12-v2"
-    },
-    "servicedesk": {
-        "default_index": "ramtin_servicedesk_index",
-        "retrieval_class": ESServiceDesk,
-        "model": "all-MiniLM-L12-v2"
-    }
+RETRIEVAL_PARAMS["servicedesk"] = {
+    "default_index": "ramtin_servicedesk_index",
+    "retrieval_class": ESServiceDesk,
+    "model": "all-MiniLM-L12-v2"
 }
