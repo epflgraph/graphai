40a686b5db23b6d2a6b69d94674a2142e2f9f14f
Francisco Pinto <francisco.pinto@epfl.ch>
2026-05-07 10:30:39 +0200

    🔧 split up workers

--- restart.sh
+++ restart.sh
@@ -2,12 +2,14 @@ echo "Restarting all services..."
 systemctl --user daemon-reload
 systemctl --user restart promtail.service
 systemctl --user restart celery-cpu-cache.service
-systemctl --user restart celery-cpu-rag.service
-systemctl --user restart celery-cpu-img.service
-systemctl --user restart celery-cpu-ontl_scrp_celery.service
-systemctl --user restart celery-cpu-text.service
 systemctl --user restart celery-cpu-embedding.service
-systemctl --user restart celery-cpu-video_voice_transl.service
+systemctl --user restart celery-cpu-image.service
+systemctl --user restart celery-cpu-ontl_scrp_celery.service
+systemctl --user restart celery-cpu-rag.service
+systemctl --user restart celery-cpu-text.service
+systemctl --user restart celery-cpu-translate.service
+systemctl --user restart celery-cpu-video.service
+systemctl --user restart celery-cpu-voice.service
 systemctl --user restart celery-gpu0-embedding.service
 systemctl --user restart celery-gpu1-voice.service
 systemctl --user restart celery-gpu2-voice.service
